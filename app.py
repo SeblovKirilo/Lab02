bd7c7e7e10cbf043bc439e4718a2fea53fb2633e
SeblovKirilo <158617546+SeblovKirilo@users.noreply.github.com>
2024-02-03 13:20:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Імпорт бібліотек
+from flask import Flask, request
 from datetime import date
 
+# Назва застосунку
+app = Flask(__name__)
+
+@app.route("/")
 def today_is():
     # Повертаємо поточну дату. У форматі ДД-ММ-РР
     return f"<p>Today is {date.today().strftime('%d-%m-%Y')}</p>"
